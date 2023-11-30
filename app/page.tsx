06dec3e411c5215992f198075b1fd0612483de0d
PiessensLander <55790048+PiessensLander@users.noreply.github.com>
2023-11-30 12:33:02 +0100

    Add pagination

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@
 import { Character } from '@/components/Character';
 import useSWR from 'swr';
 import axios from 'axios';
+import { useState } from 'react';
 
 const fetcher = (url: any) => axios.get(url).then((res) => res.data);
 
 export default function Page() {
-	const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_SWAPI_BASE_URL}/people`, fetcher);
+	const [pageIndex, setPageIndex] = useState(1);
 
+	const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_SWAPI_BASE_URL}/people?page=${pageIndex}`, fetcher);
 	if (data) {
 		console.log(data);
-	}
 
-	if (data) {
 		return (
 			<main>
 				{data &&
@@ -20,6 +20,25 @@ export default function Page() {
 						console.log(typeof item);
 						return <Character key={item.name} character={item} />;
 					})}
+
+				{data.previous ? (
+					<button
+						onClick={() => {
+							setPageIndex(pageIndex - 1);
+						}}
+					>
+						Previous page
+					</button>
+				) : null}
+				{data.next ? (
+					<button
+						onClick={() => {
+							setPageIndex(pageIndex + 1);
+						}}
+					>
+						Next page
+					</button>
+				) : null}
 			</main>
 		);
 	}
--- components/FilmTag.tsx
+++ components/FilmTag.tsx
@@ -7,10 +7,7 @@ const fetcher = (url: any) => axios.get(url).then((res) => res.data);
 
 export const FilmTag = (props: FilmTagProps) => {
 	const { data, error, isLoading } = useSWR(props.film, fetcher);
-	console.log(props.film);
-
 	if (data) {
-		console.log(data);
+		return <span className='bg-amber-600 rounded-full px-4'>{data.title}</span>;
 	}
-	return <span className='bg-amber-600 rounded-full px-4'>{data.title}</span>;
 };
